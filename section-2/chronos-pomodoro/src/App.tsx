dd9fb8228637856f995fed5aea2768a4cd04997a
MunrraMT <dev.armsantos@gmail.com>
2025-08-23 18:47:39 -0400

    19. Criando a estrutura HTML e CSS do nosso app para cada seção do site

--- section-2/chronos-pomodoro/src/App.tsx
+++ section-2/chronos-pomodoro/src/App.tsx
@@ -1,16 +1,59 @@
-import { Heading } from './components/Heading';
+import {
+  HistoryIcon,
+  HouseIcon,
+  SettingsIcon,
+  SunIcon,
+  TimerIcon,
+} from 'lucide-react';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
-    <Heading>
-      Olá mundo
-      <button>
-        <TimerIcon />
-      </button>
-    </Heading>
+    <>
+      <header className="container">
+        <h1>
+          <TimerIcon />
+          <span>Chronos</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <HouseIcon />
+            </li>
+            <li>
+              <HistoryIcon />
+            </li>
+            <li>
+              <SettingsIcon />
+            </li>
+            <li>
+              <SunIcon />
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className="container">
+        <section className="content">
+          <p>00:00</p>
+        </section>
+        <section className="content">
+          <form>
+            <p>task</p>
+            <label htmlFor="new_task">digite algo</label>
+            <input type="text" name="new_task" id="new_task" />
+            <p>
+              Próximo ciclo é de <strong>25min</strong>
+            </p>
+            <button type="submit">Iniciar nova tarefa</button>
+          </form>
+        </section>
+      </main>
+      <footer className="container">
+        <p>Entenda como funciona a técnica pomodoro</p>
+        <a href="/">Chronos Pomodoro © 2025 - Feito com 💚</a>
+      </footer>
+    </>
   );
 }
