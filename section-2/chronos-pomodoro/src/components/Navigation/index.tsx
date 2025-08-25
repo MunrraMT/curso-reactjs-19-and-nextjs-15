3628f4e6fe796f839d64cbbb0796d11f0ebf2954
MunrraMT <dev.armsantos@gmail.com>
2025-08-25 07:34:19 -0400

    37. useEffect - hook feito para trabalhar com efeitos colaterais no componente

--- section-2/chronos-pomodoro/src/components/Navigation/index.tsx
+++ section-2/chronos-pomodoro/src/components/Navigation/index.tsx
@@ -2,15 +2,32 @@ import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 
 import styles from './styles.module.css';
 import { NavigationItem } from '../NavigationItem';
+import { useEffect, useState } from 'react';
+
+type DataTheme = 'dark' | 'light';
 
 export function Navigation() {
+  const [theme, set_theme] = useState<DataTheme>('dark');
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  const change_theme = () => {
+    set_theme((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
   return (
     <nav className={styles.container}>
       <ul className={styles.list}>
-        <NavigationItem Icon={HouseIcon} />
-        <NavigationItem Icon={HistoryIcon} />
-        <NavigationItem Icon={SettingsIcon} />
-        <NavigationItem Icon={SunIcon} />
+        <NavigationItem title="Ir para home" Icon={HouseIcon} />
+        <NavigationItem title="Ver histórico" Icon={HistoryIcon} />
+        <NavigationItem title="Configurações" Icon={SettingsIcon} />
+        <NavigationItem
+          title="Trocar tema"
+          Icon={SunIcon}
+          onClick={change_theme}
+        />
       </ul>
     </nav>
   );
--- section-2/chronos-pomodoro/src/components/NavigationItem/index.tsx
+++ section-2/chronos-pomodoro/src/components/NavigationItem/index.tsx
@@ -2,15 +2,33 @@ import type { ElementType } from 'react';
 import styles from './styles.module.css';
 
 interface Props {
+  title: string;
   Icon: ElementType;
+  onClick?: () => void;
 }
 
 export function NavigationItem(props: Props) {
   return (
     <li className={styles.item}>
-      <a href="/" className={styles.link}>
-        <props.Icon className={styles.icon} />
-      </a>
+      {props.onClick ? (
+        <button
+          title={props.title}
+          aria-label={props.title}
+          className={styles.link}
+          onClick={props.onClick}
+        >
+          <props.Icon className={styles.icon} />
+        </button>
+      ) : (
+        <a
+          href="/"
+          title={props.title}
+          aria-label={props.title}
+          className={styles.link}
+        >
+          <props.Icon className={styles.icon} />
+        </a>
+      )}
     </li>
   );
 }
