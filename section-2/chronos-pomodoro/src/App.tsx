34466a0c7612a2e229dc9b28d99cae3c1907e70f
MunrraMT <dev.armsantos@gmail.com>
2025-09-03 09:09:35 -0400

    44. Estado global da aplicação com useState e comunicação via props

--- section-2/chronos-pomodoro/src/App.tsx
+++ section-2/chronos-pomodoro/src/App.tsx
@@ -1,8 +1,25 @@
+import { useState } from 'react';
+
 import { Home } from './pages/home';
+import type { TaskStateModel } from './models/TaskStateModel';
 
 import './styles/theme.css';
 import './styles/global.css';
 
+const initial_state: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
+
 export function App() {
-  return <Home />;
+  const [state, set_state] = useState(initial_state);
+  return <Home state={state} set_state={set_state} />;
 }
--- section-2/chronos-pomodoro/src/pages/home/index.tsx
+++ section-2/chronos-pomodoro/src/pages/home/index.tsx
@@ -1,8 +1,14 @@
 import { Countdown } from '../../components/Countdown';
 import { MainForm } from '../../components/MainForm';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 import { MainTemplate } from '../../templates/MainTemplate';
 
-export function Home() {
+interface Props {
+  state: TaskStateModel;
+  set_state: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+}
+
+export function Home(props: Props) {
   return (
     <MainTemplate>
       <Countdown />
