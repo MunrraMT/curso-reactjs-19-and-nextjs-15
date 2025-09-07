ab01cb90ae66cbbe72d8f0dc3083716835328458
MunrraMT <dev.armsantos@gmail.com>
2025-09-07 09:17:45 -0400

    66. Aqui está o código do seu TaskContextProvider antigo (como te prometi)

--- section-2/chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
+++ section-2/chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { initial_task_state } from './initial_task_state';
 import { TaskContext } from './TaskContext';
@@ -10,44 +10,13 @@ type TaskContextProviderProps = {
 export function TaskContextProvider(props: TaskContextProviderProps) {
   const [state, setState] = useState(initial_task_state);
 
-  const [numero, dispatch] = useReducer((state, action) => {
-    console.log({ state, action });
-
-    switch (action) {
-      case 'D':
-        return state - 1;
-
-      case 'I':
-        return state + 1;
-
-      default:
-        return state;
-    }
-  }, 0);
-
-  // useEffect(() => {
-  //   console.log(state);
-  // }, [state]);
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      <button
-        type="button"
-        onClick={() => {
-          dispatch('D');
-        }}
-      >
-        -
-      </button>
-      {numero}
-      <button
-        type="button"
-        onClick={() => {
-          dispatch('I');
-        }}
-      >
-        +
-      </button>
+      {props.children}
     </TaskContext.Provider>
   );
 }
