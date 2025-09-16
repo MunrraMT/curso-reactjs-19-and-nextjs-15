b3a0327da13dc2288d4ed89902361ab677acbbd6
MunrraMT <dev.armsantos@gmail.com>
2025-09-15 22:15:02 -0400

    67. TypeScript + Reducer: criando actions bem tipadas e escaláveis

---
+++ section-2/chronos-pomodoro/src/contexts/TaskContext/taskActions.ts
@@ -0,0 +1,22 @@
+import type { TaskModel } from '../../models/TaskModel';
+
+export const TaskActionTypes = {
+  START_TASK: 'START_TASK',
+  INTERRUPT_TASK: 'INTERRUPT_TASK',
+  RESET_STATE: 'RESET_STATE',
+} as const;
+
+export type TaskActionsWithPayload = {
+  type:
+    | typeof TaskActionTypes.START_TASK
+    | typeof TaskActionTypes.INTERRUPT_TASK;
+  payload: TaskModel;
+};
+
+export type TaskActionsWithoutPayload = {
+  type: typeof TaskActionTypes.RESET_STATE;
+};
+
+export type TaskActionModel =
+  | TaskActionsWithPayload
+  | TaskActionsWithoutPayload;
