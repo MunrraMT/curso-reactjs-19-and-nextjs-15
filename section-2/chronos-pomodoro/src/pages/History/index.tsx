b78bec6ac7ef4b4889277eb192d7992dfc4b1177
MunrraMT <dev.armsantos@gmail.com>
2025-09-20 12:29:21 -0400

    88. Reordenando a tabela do histórico ao clicar th (no cabeçalho)

--- section-2/chronos-pomodoro/src/pages/History/index.tsx
+++ section-2/chronos-pomodoro/src/pages/History/index.tsx
@@ -1,21 +1,44 @@
-import { TrashIcon } from 'lucide-react';
+import { useState } from 'react';
+import { ArrowUpDownIcon, TrashIcon } from 'lucide-react';
 
 import { ButtonCustom } from '../../components/ButtonCustom';
 import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
 import { Section } from '../../components/Section';
 import { useTaskContext } from '../../contexts/TaskContext';
-
-import styles from './styles.module.css';
+import { sortTasks, type SortTasksOptions } from '../../utils/sortTasks';
 import type { TaskModel } from '../../models/TaskModel';
 import { formatDate } from '../../utils/formatDate';
 import { getTaskStatus } from '../../utils/getTaskStatus';
-import { sortTasks } from '../../utils/sortTasks';
+
+import styles from './styles.module.css';
 
 export function History() {
   const { state } = useTaskContext();
+  const [sortTaskOptions, setSortTaskOptions] = useState<SortTasksOptions>(
+    () => {
+      return {
+        tasks: sortTasks({ tasks: state.tasks }),
+        field: 'startDate',
+        direction: 'desc',
+      };
+    },
+  );
 
-  const sortedTask = sortTasks({ tasks: state.tasks });
+  function handleSortTasks({ field }: Pick<SortTasksOptions, 'field'>) {
+    const newDirection: SortTasksOptions['direction'] =
+      sortTaskOptions.direction === 'desc' ? 'asc' : 'desc';
+
+    setSortTaskOptions({
+      tasks: sortTasks({
+        direction: newDirection,
+        tasks: sortTaskOptions.tasks,
+        field,
+      }),
+      direction: newDirection,
+      field,
+    });
+  }
 
   const taskTypeDictionary: Record<TaskModel['type'], string> = {
     workTime: 'Foco',
@@ -41,15 +64,39 @@ export function History() {
           <table>
             <thead>
               <tr>
-                <th>Tarefa</th>
-                <th>Duração</th>
-                <th>Data</th>
+                <th
+                  className={styles.thSort}
+                  onClick={() => handleSortTasks({ field: 'name' })}
+                >
+                  <span className={styles.thSortContent}>
+                    Tarefa
+                    <ArrowUpDownIcon className={styles.thSortIcon} />
+                  </span>
+                </th>
+                <th
+                  className={styles.thSort}
+                  onClick={() => handleSortTasks({ field: 'duration' })}
+                >
+                  <span className={styles.thSortContent}>
+                    Duração
+                    <ArrowUpDownIcon className={styles.thSortIcon} />
+                  </span>
+                </th>
+                <th
+                  className={styles.thSort}
+                  onClick={() => handleSortTasks({ field: 'startDate' })}
+                >
+                  <span className={styles.thSortContent}>
+                    Data
+                    <ArrowUpDownIcon className={styles.thSortIcon} />
+                  </span>
+                </th>
                 <th>Status</th>
                 <th>Tipo</th>
               </tr>
             </thead>
             <tbody>
-              {sortedTask.map((task) => (
+              {sortTaskOptions.tasks.map((task) => (
                 <tr key={task.id}>
                   <td align="center">{task.name}</td>
                   <td align="center">{task.duration}min</td>
