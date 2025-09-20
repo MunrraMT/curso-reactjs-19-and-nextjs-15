31cd8cc9858d3054b975563c62819a3c8ddad3ba
MunrraMT <dev.armsantos@gmail.com>
2025-09-20 13:52:36 -0400

    93. Capturando o onSubmit (envio) do formulário com ref e useRef dos inputs

--- section-2/chronos-pomodoro/src/components/InputCustom/index.tsx
+++ section-2/chronos-pomodoro/src/components/InputCustom/index.tsx
@@ -1,4 +1,5 @@
 import type { ComponentProps } from 'react';
+
 import styles from './styles.module.css';
 
 interface Props extends ComponentProps<'input'> {
--- section-2/chronos-pomodoro/src/pages/Settings/index.tsx
+++ section-2/chronos-pomodoro/src/pages/Settings/index.tsx
@@ -9,9 +9,25 @@ import { useTaskContext } from '../../contexts/TaskContext';
 import styles from './styles.module.css';
 import { ButtonCustom } from '../../components/ButtonCustom';
 import { SaveIcon } from 'lucide-react';
+import { useRef } from 'react';
+import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 
 export function Settings() {
-  const { state } = useTaskContext();
+  const { state, dispatch } = useTaskContext();
+
+  const workTimeInputRef = useRef<HTMLInputElement>(null);
+  const shortBreakTimeInputRef = useRef<HTMLInputElement>(null);
+  const longBreakTimeInputRef = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const workTime = workTimeInputRef.current?.value;
+    const shortBreakTime = shortBreakTimeInputRef.current?.value;
+    const longBreakTime = longBreakTimeInputRef.current?.value;
+
+    // dispatch({type: TaskActionTypes.})
+  }
 
   return (
     <MainTemplate>
@@ -25,36 +41,39 @@ export function Settings() {
       </Section>
 
       <Section>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <div className={styles.formRow}>
             <InputCustom
+              ref={workTimeInputRef}
               label={taskTypeDictionary.workTime}
               id={taskTypeConstant.workTime}
               name={taskTypeConstant.workTime}
               placeholder="Digite algo"
-              type="text"
+              type="number"
               defaultValue={state.config.workTime}
             />
           </div>
 
           <div className={styles.formRow}>
             <InputCustom
+              ref={shortBreakTimeInputRef}
               label={taskTypeDictionary.shortBreakTime}
               id={taskTypeConstant.shortBreakTime}
               name={taskTypeConstant.shortBreakTime}
               placeholder="Digite algo"
-              type="text"
+              type="number"
               defaultValue={state.config.shortBreakTime}
             />
           </div>
 
           <div className={styles.formRow}>
             <InputCustom
+              ref={longBreakTimeInputRef}
               label={taskTypeDictionary.longBreakTime}
               id={taskTypeConstant.longBreakTime}
               name={taskTypeConstant.longBreakTime}
               placeholder="Digite algo"
-              type="text"
+              type="number"
               defaultValue={state.config.longBreakTime}
             />
           </div>
