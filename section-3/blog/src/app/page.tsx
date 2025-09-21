1fc8114a1f9515c6e4d127f3f33127e839f5f59e
MunrraMT <dev.armsantos@gmail.com>
2025-09-21 17:19:21 -0400

    114. Container geral do site + tema claro e escuro no Tailwind CSS

--- section-3/blog/src/app/page.tsx
+++ section-3/blog/src/app/page.tsx
@@ -1,19 +1,12 @@
 import { Suspense } from 'react';
 
-import { Heading } from '@/components/Heading';
 import PostList from '@/components/PostList';
 import { SpinLoader } from '@/components/SpinLoader';
 
 export default async function HomePage() {
   return (
-    <>
-      <Suspense fallback={<SpinLoader className="min-h-full" />}>
-        <PostList />
-      </Suspense>
-
-      <header>
-        <Heading text="Header" />
-      </header>
+    <div className="text-slate-900 dark:text-slate-100 bg-slate-100 dark:bg-slate-900 min-h-screen">
+      <header>Aqui é o header</header>
 
       <main>
         <Suspense fallback={<SpinLoader className="min-h-full" />}>
@@ -21,13 +14,7 @@ export default async function HomePage() {
         </Suspense>
       </main>
 
-      <footer>
-        <Heading text="Footer" />
-      </footer>
-
-      <Suspense fallback={<SpinLoader className="min-h-full" />}>
-        <PostList />
-      </Suspense>
-    </>
+      <footer>Footer</footer>
+    </div>
   );
 }
