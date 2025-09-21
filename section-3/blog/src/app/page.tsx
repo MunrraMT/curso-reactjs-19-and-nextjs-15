59d9ab3f22d61128b6dbf6cc234fc0617f518123
MunrraMT <dev.armsantos@gmail.com>
2025-09-20 20:57:43 -0400

    105. Sobre o Tailwind CSS, clsx e extensões do VS Code para facilitar sua vida

--- section-3/blog/src/app/page.tsx
+++ section-3/blog/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function HomePage() {
-  return <h1>Olá dentro da página</h1>;
+  return (
+    <h1 className="text-xl font-bold w-full text-blue-500 hover:text-blue-100 bg-blue-100 hover:bg-blue-500 transition duration-300 text-center uppercase py-4">
+      Olá dentro da página
+    </h1>
+  );
 }
