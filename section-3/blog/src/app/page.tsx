a71aa786612d24c19947863452550dd828c4de91
MunrraMT <dev.armsantos@gmail.com>
2025-09-22 22:56:14 -0400

    fix

--- section-3/blog/src/app/page.tsx
+++ section-3/blog/src/app/page.tsx
@@ -20,6 +20,25 @@ export default async function HomePage() {
           containerLinkProps={{ href: '#' }}
           imageProps={{ alt: '', src: '/images/bryen_0.png' }}
         />
+
+        <div className={clsx('flex flex-col gap-2', 'sm:justify-center')}>
+          <time dateTime="2025-04-20" className="text-slate-600 text-sm/tight">
+            20/04/2025
+          </time>
+
+          <PostHeading as="h1" href="#">
+            Lorem ipsum dolor sit, amet consectetur
+          </PostHeading>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est,
+            minima quasi. Aliquid hic provident tempore molestiae recusandae
+            impedit aspernatur, enim dolor corrupti et sint natus, possimus,
+            quia nemo magnam. Iusto, quia consequatur! Alias quas perspiciatis
+            beatae, et error, nobis, optio dicta enim modi natus voluptate
+            aliquam iusto itaque ipsa magni.
+          </p>
+        </div>
       </section>
 
       <main>
