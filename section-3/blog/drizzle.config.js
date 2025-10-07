b8c665277fc371d6faf55d7a6b9217c83a309ca7
MunrraMT <dev.armsantos@gmail.com>
2025-10-07 00:42:47 -0400

    147. Configurando o Drizzle o nosso primeiro Schema (tabela da base de dados)

---
+++ section-3/blog/drizzle.config.js
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './src/db/drizzle/migrations',
+  schema: './src/db/drizzle/schemas.ts',
+  dialect: 'sqlite',
+  dbCredentials: {
+    url: './db.sqlite3',
+  },
+});
